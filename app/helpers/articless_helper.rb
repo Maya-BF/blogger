b79f7100bed6779ef83515e8aedba8bafae7a454
Maya <maya@Mariems-MacBook-Pro.local>
2016-02-20 14:47:20 -0500

    first blogger commit

---
+++ app/helpers/articless_helper.rb
@@ -0,0 +1,2 @@
+module ArticlessHelper
+end
---
+++ test/controllers/articless_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ArticlessControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
